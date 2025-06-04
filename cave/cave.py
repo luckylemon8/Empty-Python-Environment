dba702fa2f089769e3971f43e94f34eda82752f9
luckylemon8 <133763365+luckylemon8@users.noreply.github.com>
2025-06-04 02:04:49 +0000

    challenge 16

--- cave/cave.py
+++ cave/cave.py
@@ -4,11 +4,34 @@ class Cave:
         self.description = None
         self.linked_caves = {}
 
-    def get_description(self):
-        return self.description
-
     def set_description(self, cave_description):
         self.description = cave_description
 
+    def get_description(self):
+        return self.description
+
+    def set_name(self, cave_name):
+        self.name = cave_name
+    
+    def get_name(self):
+        return self.name
+
+    def set_character(self, new_character):
+        self.character = new_character
+    
+    def get_character(self):
+        return self.character
+
     def describe(self):
         print(self.description)
+    
+    def link_cave(self, cave_to_link, direction):
+        self.linked_caves[direction] = cave_to_link
+
+    def get_details(self):
+        print("The " + self.name)
+        print("----------")
+        print(self.description)
+        for direction in self.linked_caves:
+            cave = self.linked_caves[direction]
+            print("The " + cave.get_name() + " is " + direction)
--- cave/main.py
+++ cave/main.py
@@ -3,6 +3,16 @@ from cave import Cave
 cavern = Cave("cavern")
 grotto = Cave("grotto")
 dungeon = Cave("dungeon")
-cavern.set_description("A damp and dirty cave.")
+sinkhole = Cave("sinkhole")
 
-cavern.describe()
+cavern.set_description("A damp and dirty cave, with various items scattered on the floor.")
+grotto.set_description("A small cave with ancient graffiti.")
+dungeon.set_description("A closed off cave with a moss-covered door.")
+
+cavern.link_cave(dungeon, 'south')
+cavern.link_cave(sinkhole, 'North')
+grotto.link_cave(dungeon, 'east')
+dungeon.link_cave(grotto, 'west')
+
+cavern.get_details()
+
